344a836a2880f577ed7102db5690e695f4f7119c
Aditya P <75028517+AdityaP029@users.noreply.github.com>
2023-07-20 22:56:37 +0530

    Update code v1.ino

--- code v1.ino
+++ code v1.ino
@@ -2,32 +2,30 @@
 #include <SPI.h>
 #include <RF24Audio.h>
 #include "printf.h"		// General includes for radio and audio lib
-RF24 radio (7, 8);		// Set radio up using pins 7 (CE) 8 (CS)
-RF24Audio rfAudio (radio, 0);	// Set up the audio using the radio, and set to radio number 0
+RF24 radio(7, 8);		// Set radio up using pins 7 (CE) 8 (CS)
+RF24Audio rfAudio(radio, 0);	// Set up the audio using the radio, and set to radio number 0
 int talkButton = 3;
-void
-setup ()
+void setup()
 {
-  Serial.begin (115200);
-  printf_begin ();
-  radio.begin ();
-  radio.printDetails ();
-  rfAudio.begin ();
-  pinMode (talkButton, INPUT);	//sets interrupt to check for button talk abutton press
+  Serial.begin(115200);
+  printf_begin();
+  radio.begin();
+  radio.printDetails();
+  rfAudio.begin();
+  pinMode(talkButton, INPUT);	//sets interrupt to check for button talk abutton press
   void talk();
-  attachInterrupt (digitalPinToInterrupt (talkButton), talk, CHANGE);	//sets the default state for each module to receive
-  rfAudio.receive ();
+  attachInterrupt(digitalPinToInterrupt(talkButton), talk, CHANGE);	//sets the default state for each module to receive
+  rfAudio.receive();
 }
 
-void talk ()
+void talk()
 {
-  if (digitalRead (talkButton))
-    rfAudio.transmit ();
+  if (digitalRead(talkButton))
+    rfAudio.transmit();
   else
-    rfAudio.receive ();
+    rfAudio.receive();
 }
 
-void
-loop ()
+void loop()
 {
 }
